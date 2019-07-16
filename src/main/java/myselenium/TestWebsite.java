7059fff374995dc32fd298608a13340902ca80b5
Nisrin Dhoondia <38102371+mygoal-javadeveloper@users.noreply.github.com>
2019-07-16 10:24:12 +0530

    Update TestWebsite.java

--- src/main/java/myselenium/TestWebsite.java
+++ src/main/java/myselenium/TestWebsite.java
@@ -8,9 +8,9 @@ public class TestWebsite {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "/Users/HP/eclipse-workspace1/myseleniumclasses/src/main/java/driver/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/.../.../.../myseleniumclasses/src/main/java/driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("file:///C:/Users/HP/eclipse-workspace1/myseleniumclasses/HelloWorld.HTML");
+		driver.get("file:///.../.../.../.../myseleniumclasses/HelloWorld.HTML");
 		System.out.println(driver.getTitle());
 		WebElement message = driver.findElementByTagName("h1");
 		System.out.println(message.getText());
